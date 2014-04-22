5b1cf9ae012624fffeee6820db951ebf75b70a35
sierramike <sierramike@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-22 07:42:19 +0000

    First release of MoreAboutMe widget.
    git-svn-id: https://plugins.svn.wordpress.org/moreaboutme/trunk@899919 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ moreaboutme-widget.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * @package MoreAboutMe_widget
+ * @version 1.0
+ */
+/*
+  Plugin Name: MoreAboutMe Widget
+  Plugin URI: http://wordpress.org/plugins/moreaboutme-widget
+  Description: Displays an AboutMe bloc, including a picture and some text
+  Version: 1.0
+  Author: Stéphane Moitry
+  Author URI: http://stephane.moitry.fr
+  License: GPL2
+ */
+
+/*  Copyright 2013-2014  Stéphane Moitry (stephane.moitry@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * moreaboutme_widget_Widget Class
+ */
+class moreaboutme_widget_Widget extends WP_Widget {
+
+	/** constructor */
+	function moreaboutme_widget_Widget() {
+		parent::WP_Widget(false, 'MoreAboutMe Widget', array('description' => 'Displays an AboutMe bloc, including a picture and some text'));
+		$control_ops = array('width' => 400, 'height' => 350);
+	}
+
+	/** @see WP_Widget::widget */
+	function widget($args, $instance) {
+		extract($args);
+		$title = apply_filters('widget_title', $instance['title']);
+		$texte = esc_attr($instance['texte']);
+		$imageurl = esc_attr($instance['imageurl']);
+		$filter = $instance['filter'];
+
+		echo $before_widget;
+
+		if ($title) {
+			echo $before_title . $title . $after_title;
+		}
+
+		echo $this->render($texte, $imageurl, $filter);
+
+		echo $after_widget;
+	}
+
+	/* Render the content, used by widget and shortcode methods */
+	public static function render($texte, $imageurl, $filter) {
+		$text = '';
+		
+		if ($imageurl != '') {
+				$text = $text . '<div class="moreaboutme_img"><img src="' . $imageurl .'"></div>';
+		}
+
+		$text = $text . '<div class="moreaboutme_txt">' . (!empty( $filter ) ? wpautop( $texte ) : $texte) . '</div>';
+		
+		return $text;
+	}
+
+	/** @see WP_Widget::update */
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		if ( current_user_can('unfiltered_html') )
+			$instance['texte'] =  $new_instance['texte'];
+		else
+			$instance['texte'] = stripslashes( wp_filter_post_kses( addslashes($new_instance['texte']) ) ); // wp_filter_post_kses() expects slashed
+		$instance['filter'] = isset($new_instance['filter']);
+		$instance['imageurl'] = strip_tags($new_instance['imageurl']);
+		return $instance;
+	}
+
+	/** @see WP_Widget::form */
+	function form($instance) {
+		$title = '';
+		$texte = '';
+		$imageurl = '';
+
+		if (isset($instance['title'])) {
+			$title = esc_attr($instance['title']);
+		}
+		
+		if (isset($instance['texte'])) {
+			$texte = esc_textarea($instance['texte']);
+		}
+		
+		if (isset($instance['imageurl'])) {
+			$imageurl = esc_attr($instance['imageurl']);
+		}
+?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('texte'); ?>"><?php _e('Text:'); ?> <textarea class="widefat" class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('texte'); ?>" name="<?php echo $this->get_field_name('texte'); ?>"><?php echo $texte; ?></textarea></label></p>
+		<p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox" <?php checked(isset($instance['filter']) ? $instance['filter'] : 0); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatically add paragraphs'); ?></label></p>
+		<p><label for="<?php echo $this->get_field_id('imageurl'); ?>"><?php _e('Image Url:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('imageurl'); ?>" name="<?php echo $this->get_field_name('imageurl'); ?>" type="text" value="<?php echo $imageurl; ?>" /></label></p>
+<?php
+	}
+
+}
+
+/* ShortCode Handler */
+function moreaboutme_widget_shortcode( $atts ) {
+	$attributes = shortcode_atts( array(
+	    'texte' => '',
+	    'imageurl' => ''
+	), $atts );
+	
+	$text = "<div class='widget_moreaboutme_widget'>".moreaboutme_widget_Widget::render($attributes['texte'], $attributes['imageurl'])."</div>";
+	
+	return $text;
+}
+
+/* Initialization Handler */
+function moreaboutme_widget_init() {
+	register_widget( 'moreaboutme_widget_Widget' );
+	wp_enqueue_style( 'moreaboutme_widget_Widget', plugin_dir_url( __FILE__ ).'moreaboutme-widget.css');
+}
+
+// register Widget
+add_action('widgets_init', 'moreaboutme_widget_init');
+// register ShortCode
+add_shortcode( 'moreaboutme', 'moreaboutme_widget_shortcode' );
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== MoreAboutMe Widget ===
+Contributors: sierramike
+Tags: picture, image, about, author, widget
+Requires at least: 2.8
+Tested up to: 3.9
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Displays an AboutMe bloc, including a picture and some text
+
+== Description ==
+
+Displays an AboutMe bloc, including a picture and some text.
+
+== Installation ==
+
+1. Upload the zipfile to the `/wp-content/plugins/` directory
+2. Extract and remove it
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Drag the new widget to desired sidebar, choose a title (or not) for the widget and fill additional properties.
+
+== Screenshots ==
+
+1. Admin side of things.
+2. Sample rendering.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
