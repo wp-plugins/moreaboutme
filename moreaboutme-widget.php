6ea10c0c0abaf4b6118d6ad062eb1bd95a4e3860
sierramike <sierramike@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-18 14:23:32 +0000

    Added translation capabilities.
    git-svn-id: https://plugins.svn.wordpress.org/moreaboutme/trunk@916590 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- moreaboutme-widget.php
+++ moreaboutme-widget.php
@@ -1,16 +1,18 @@
 <?php
 /**
  * @package MoreAboutMe_widget
- * @version 1.1.1
+ * @version 1.2
  */
 /*
   Plugin Name: MoreAboutMe Widget
   Plugin URI: http://wordpress.org/plugins/moreaboutme
   Description: Displays an AboutMe bloc, also known as a More About Me bloc, including a picture and some text. This plug-in is useful if you want to put a widget in a sidebar or footer to show a picture of your own and some descriptive text. Check the screenshots to see if it could fit your needs.
-  Version: 1.1.1
+  Version: 1.2
   Author: Stéphane Moitry
   Author URI: http://stephane.moitry.fr
   License: GPL2
+  Text Domain: moreaboutme-widget
+  Domain Path: /languages/
  */
 
 /*  Copyright 2013-2014  Stéphane Moitry (stephane.moitry@gmail.com)
@@ -36,7 +38,7 @@ class moreaboutme_widget_Widget extends WP_Widget {
 
 	/** constructor */
 	function moreaboutme_widget_Widget() {
-		parent::WP_Widget(false, 'MoreAboutMe Widget', array('description' => 'Displays an AboutMe bloc, including a picture and some text'));
+		parent::WP_Widget(false, 'MoreAboutMe Widget', array('description' => __('Displays an AboutMe bloc, including a picture and some text.', 'moreaboutme-widget')));
 		$control_ops = array('width' => 400, 'height' => 350);
 	}
 
@@ -103,10 +105,10 @@ class moreaboutme_widget_Widget extends WP_Widget {
 			$imageurl = esc_attr($instance['imageurl']);
 		}
 ?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
-		<p><label for="<?php echo $this->get_field_id('texte'); ?>"><?php _e('Text:'); ?> <textarea class="widefat" class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('texte'); ?>" name="<?php echo $this->get_field_name('texte'); ?>"><?php echo $texte; ?></textarea></label></p>
-		<p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox" <?php checked(isset($instance['filter']) ? $instance['filter'] : 0); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatically add paragraphs'); ?></label></p>
-		<p><label for="<?php echo $this->get_field_id('imageurl'); ?>"><?php _e('Image Url:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('imageurl'); ?>" name="<?php echo $this->get_field_name('imageurl'); ?>" type="text" value="<?php echo $imageurl; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'moreaboutme-widget'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('texte'); ?>"><?php _e('Text:', 'moreaboutme-widget'); ?> <textarea class="widefat" class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('texte'); ?>" name="<?php echo $this->get_field_name('texte'); ?>"><?php echo $texte; ?></textarea></label></p>
+		<p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox" <?php checked(isset($instance['filter']) ? $instance['filter'] : 0); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatically add paragraphs', 'moreaboutme-widget'); ?></label></p>
+		<p><label for="<?php echo $this->get_field_id('imageurl'); ?>"><?php _e('Image Url:', 'moreaboutme-widget'); ?> <input class="widefat" id="<?php echo $this->get_field_id('imageurl'); ?>" name="<?php echo $this->get_field_name('imageurl'); ?>" type="text" value="<?php echo $imageurl; ?>" /></label></p>
 <?php
 	}
 
@@ -126,6 +128,7 @@ function moreaboutme_widget_shortcode( $atts ) {
 
 /* Initialization Handler */
 function moreaboutme_widget_init() {
+	load_plugin_textdomain( 'moreaboutme-widget', false, dirname( plugin_basename( __FILE__ ) ).'/languages' );
 	register_widget( 'moreaboutme_widget_Widget' );
 	wp_enqueue_style( 'moreaboutme_widget_Widget', plugin_dir_url( __FILE__ ).'moreaboutme-widget.css');
 }
